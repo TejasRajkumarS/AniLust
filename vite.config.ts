33c096b39148c9f254bc8d7d02559e42f36515ca
Tejas <tejasrajkumar.s@gmail.com>
2026-02-08 19:28:25 +0530

    Fix: Correct base path for Vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // FIX: Use the repository name surrounded by slashes
+      base: '/AniLust/', 
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
